176c2a59851ceebb719190f7dfa3e44d48e0b404
wls503pl <peile.wu.1990@gmail.com>
2026-04-16 11:59:49 +0800

    feat: initialize popup.js

---
+++ popup.js
@@ -0,0 +1,77 @@
+/**
+ * This file builds all the features and functionalities for the extension, including API calls, 
+ * generating APIs, private keys, passwords, and more. It is a critical component of the wallet.
+ */
+document.addEventListener("DOMContentLoaded", function() {
+    // Target all the UI elements here
+})
+
+// State Variables
+// Manage state variables to support multiple Accounts and Networks
+let providerURL = '';
+
+let provider;
+let privateKey;
+let address;
+
+// Functions
+function handler() {};
+
+function checkBalance() {};
+
+// Opens the network selection component
+function getOpenNetwork() {};
+
+// Retrieves the currently selected network to connect to
+function getSelectedNetwork() {};
+
+function setNetwork() {};
+
+// Authenticates the user and allows them to log into the application
+function loginUser() {};
+
+function createUser() {};
+
+// Opens the account creation modal for users to input credentials and create a new account
+function openCreate() {};
+
+function signUp() {};
+
+function login() {};
+
+function logout() {};
+
+// Opens the token transfer component
+function openTransfer() {};
+
+// Navigates back to the previous component or view
+function goBack() {};
+
+function openImport() {};
+
+function importGoBack() {};
+
+function openActivity() {};
+
+// Displays all assets fetched from the database or manually entered by the user in the wallet
+function openAssets() {};
+
+// Navigates the user back to the home page
+function goHomePage() {};
+
+function openImportModel() {};
+
+function closeImportModel() {};
+
+function addToken() {};
+
+function addAccount() {};
+
+// Reloads all wallet data when the extension is opened
+function myFunction() {};
+
+// Allows the user to copy their wallet address to the clipboard
+function copyAddress() {};
+
+// Allows the user to switch between different wallet accounts
+function changeAccount() {};
